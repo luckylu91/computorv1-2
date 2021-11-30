a586cb63d76776d0ebc8b07c2fb74c4e89919a24
Lucas Zins <zins.lucas@gmail.com>
2021-11-30 16:28:50 +0100

    push

--- computorv2/tokenizer.py
+++ computorv2/tokenizer.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
+
 import re
 
-tokens = [r'\(', '\)', '\+', '\-', ]
-token_pattern = r"\d+|"
+tokens = [r'\(', r'\)', r'\[', r'\]', ',', ';', r'\+', r'\-', r'\*', '/', '%', '=', r'\w+', r'\d+']
+token_pattern = '|'.join(tokens)
+
+if __name__ == '__main__':
+    toks = re.findall(token_pattern, "funB(y) = 43 * y / (4 % 2 * y)")
+    print(toks)
+    toks = re.findall(token_pattern, "varA = [[2,3];[4,3]]")
+    print(toks)
