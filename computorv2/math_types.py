0b6601b5f066b41faf5bf3d19fa545946ace8aee
Lucas Zins <zins.lucas@gmail.com>
2021-12-01 19:27:50 +0100

    push

---
+++ computorv2/math_types.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python3
+
+from typing import Union
+from math_utils import pgcd, ppcm, reduce_fraction
+
+class Rational:
+    def __init__(self, num: int, denum: int = 1):
+        num, denum = reduce_fraction(num, denum)
+        self.num = num
+        self.denum = denum
+
+    @classmethod
+    def zero(cl):
+        return Rational(0)
+
+    @classmethod
+    def one(cl):
+        return Rational(1)
+
+    def __add__(self, other):
+        if isinstance(other, Complex):
+            return other + self
+        new_denum = ppcm(self.denum, other.denum)
+        return Rational(self.num * new_denum // self.denum + other.num * new_denum // other.denum, new_denum)
+
+    def __neg__(self):
+        return Rational(-self.num, self.denum)
+
+    def __sub__(self, other):
+        if isinstance(other, Complex):
+            return other - self
+        return self + (-other)
+
+    def __mul__(self, other):
+        if isinstance(other, Complex):
+            return other * self
+        return Rational(self.num * other.num, self.denum * other.denum)
+
+    # def __invert__(self):
+    #     return Rational(self.denum, self.num)
+
+    def __truediv__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self, 0) / other
+        return Rational(self.num * other.denum, self.denum * other.num)
+
+    def __mod__(self, other):
+        return Rational((self.num * other.denum) % (other.num * self.denum), self.denum * other.denum)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Rational):
+            return self.num == other.num and self.denum == other.denum
+        elif isinstance(other, Complex):
+            return other.im == 0 and self == other.re
+        else:
+            assert(isinstance(other, int))
+            return self.denum == 1 and self.num == other
+
+    def __radd__(self, other):
+        return self + other
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __req__(self, other) -> bool:
+        return self == other
+
+    def __rtruediv__(self, other):
+        return self / other
+
+    def __rmod__(self, other):
+        return self % other
+
+    def __str__(self):
+        if self.denum == 1:
+            return str(self.num)
+        return f"{self.num / self.denum:.6f}".rstrip('0').rstrip('.')
+
+class Complex:
+    def __init__(self, re: Union[int, Rational], im: Union[int, Rational] = 0):
+        self.re = re if isinstance(re, Rational) else Rational(re)
+        self.im = im if isinstance(im, Rational) else Rational(im)
+
+    @classmethod
+    def zero(cl):
+        return Complex(0, 0)
+
+    @classmethod
+    def one(cl):
+        return Complex(1, 0)
+
+    @classmethod
+    def i(cl):
+        return Complex(0, 1)
+
+    @classmethod
+    def from_any_value(cl, val):
+        if (isinstance(val, Complex)):
+            return val
+        if (isinstance(val, Rational) or isinstance(val, int)):
+            return Complex(val)
+        raise Exception()
+
+    def __add__(self, other):
+        other = Complex.from_any_value(other)
+        return Complex(self.re + other.re, self.im + other.im)
+
+    def __neg__(self):
+        return Complex(-self.re, -self.im)
+
+    def __sub__(self, other):
+        other = Complex.from_any_value(other)
+        return self + (-other)
+
+    def __mul__(self, other):
+        other = Complex.from_any_value(other)
+        return Complex(self.re * other.re - self.im * other.im,
+                       self.re * other.im + self.im * other.re)
+
+    def _inverse(self):
+        d = self.re * self.re + self.im * self.im
+        return Complex(self.re / d, -self.im / d)
+
+    def __truediv__(self, other):
+        other = Complex.from_any_value(other)
+        return self * other._inverse()
+
+    def __eq__(self, other) -> bool:
+        other = Complex.from_any_value(other)
+        return self.re == other.re and self.im == other.im
+
+    def __radd__(self, other):
+        return self + other
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __req__(self, other) -> bool:
+        return self == other
+
+    def __rtruediv__(self, other):
+        return self._inverse() * other
+
+    def __str__(self):
+        if self.im == 0:
+            return str(self.re)
+        if self.re == 0:
+            return str(self.im) + "i"
+        return f"{self.re} + {self.im}i"
+
+class Matrix:
+    @classmethod
+    def shape_is_valid(cl, data):
+        return len(set(len(line) for line in data)) == 1
+
+    def __init__(self, data):
+        if not Matrix.shape_is_valid(data):
+            raise Exception()
+        self.data = data
+        self.h = len(data)
+        self.w = len(data[0])
+
+    @classmethod
+    def verify_same_shape(cl, m1, m2):
+        if not (m1.h == m2.h and m1.w == m2.w):
+            raise Exception()
+
+    @classmethod
+    def verify_can_mult(cl, m1, m2):
+        if m1.w != m2.h:
+            raise Exception()
+
+    @classmethod
+    def matmult(cl, m1, m2):
+        Matrix.verify_can_mult(m1, m2)
+        res = []
+        for i, row1 in enumerate(m1.data):
+            res_row = []
+            for j, el1 in enumerate(row1):
+                el2 = m2.data[j][i]
+                res_row.append(el1 * el2)
+            res.append(res_row)
+        return res
+
+    @classmethod
+    def elementwise_operation(cl, operation, m1, m2):
+        return Matrix([[operation(el1, el2) for el1, el2 in zip(row1, row2)] for row1, row2 in zip(m1.data, m2.data)])
+
+    @classmethod
+    def elementwise_unary_operation(cl, operation, m1):
+        return Matrix([[operation(el1) for el1 in row1] for row1 in m1.data])
+
+    def __add__(self, other):
+        if isinstance(other, Matrix):
+            Matrix.verify_same_shape(self, other)
+            return Matrix.elementwise_operation(lambda x, y: x + y, self, other)
+        else:
+            return Matrix.elementwise_unary_operation(lambda x: x + other, self)
+
+    def __neg__(self):
+        return Matrix.elementwise_unary_operation(lambda x: -x, self)
+
+    def __sub__(self, other):
+        if isinstance(other, Matrix):
+            Matrix.verify_same_shape(self, other)
+            return Matrix.elementwise_operation(lambda x, y: x - y, self, other)
+        else:
+            return Matrix.elementwise_unary_operation(lambda x: x - other, self)
+
+    def __mul__(self, other):
+        if isinstance(other, Matrix):
+            raise Exception()
+        else:
+            return Matrix.elementwise_unary_operation(lambda x: x * other, self)
+
+    def __truediv__(self, other):
+        if isinstance(other, Matrix):
+            raise Exception()
+        else:
+            return Matrix.elementwise_unary_operation(lambda x: x / other, self)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Matrix):
+            Matrix.verify_same_shape(self, other)
+            return all(Matrix.elementwise_operation(lambda x, y: x == y, self, other).data)
+        else:
+            return Matrix.elementwise_unary_operation(lambda x: x / other, self)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __req__(self, other) -> bool:
+        return self == other
+
+    def __str__(self):
+        return "\n".join("[" + ",".join(f" {l} " for l in line) + "]" for line in self.data)
+
+
+
+def tests_complex():
+    c = Complex(1)
+    print(c)
+    c = Complex(1, 1)
+    print(c)
+    c = Complex(Rational(1, 2), Rational(22, 21))
+    print(c)
+    c = Complex(1, 1) * Complex(1, 1)
+    print(c)
+    c = 5 + 2 * Complex.i()
+    print(c)
+    c = (1 + Complex.i()) / (1 + Complex.i())
+    print(c)
+    c = (1 + Complex.i()) / 2
+    print(c)
+    c = 1 / (1 + Complex.i())
+    print(c)
+    c = 1 / Complex.i()
+    print(c)
+
+
+def tests_rationals():
+    r = Rational(12, 1)
+    print(r)
+    r = Rational(12, 6)
+    print(r)
+    r = Rational(1, 3)
+    print(r)
+    r = Rational(1, 3) + Rational(1, 2)
+    print(r)
+    r = Rational(1, 3) - Rational(1, 2)
+    print(r)
+    r = Rational(1, 3) * Rational(1, 2)
+    print(r)
+    r = Rational(1, 3) / Rational(1, 2)
+    print(r)
--- computorv2/tokenizer.py
+++ computorv2/tokenizer.py
@@ -1,15 +1,119 @@
 #!/usr/bin/env python3
 
 import re
+from math_types import Rational
 
-tokens = [r'\(', r'\)', r'\[', r'\]', ',', ';', r'\+', r'\-', r'\*', '/', '%', '=', r'\w+', r'\d+']
-token_pattern = '|'.join(tokens)
-
+tokens_patterns = [
+    r'\(', r'\)', r'\[', r'\]', ',', ';',
+    r'\+', r'\-', r'\*', r'\*\*', '/', '%', '=',
+    r'[a-zA-Z]+', r'\d+(?:\.\d*)?'
+]
+token_pattern = '|'.join(tokens_patterns)
 def tokenize(line):
     return re.findall(token_pattern, line)
 
+is_variable = lambda tok: (re.fullmatch(r'[a-zA-Z]+', tok) != None)
+is_number = lambda tok: (re.fullmatch(r'\d+(\.\d*)?', tok) != None)
+is_literal = lambda tok: (is_variable(tok) or is_number(tok))
+
+def literal_to_rational(tok: str):
+    tok = tok.rstrip('0').rstrip('.')
+    if tok == '':
+        return Rational(0)
+    elif '.' not in tok:
+        return Rational(int(tok))
+    else:
+        int_part, dec_part = tok.split('.')
+        pow_ten = 10 ** len(dec_part)
+        int_part, dec_part = int(int_part), int(dec_part)
+        return Rational(int_part * pow_ten + dec_part, pow_ten)
+
+class Token:
+    LITERAL = "LITERAL"
+    VARIABLE = "VARIABLE"
+    FUNCTION = "FUNCTION"
+    PLUS = "PLUS"
+    MINUS = "MINUS"
+    MULT = "MULT"
+    MATMULT = "MATMULT"
+    DIV = "DIV"
+    MOD = "MOD"
+    LPAR = "LPAR"
+    RPAR = "RPAR"
+    LBRACK = "LBRACK"
+    RBRACK = "RBRACK"
+    COMA = "COMA"
+    SEMICOL = "SEMICOL"
+    EQUAL = "EQUAL"
+    EOF = "EOF"
+
+    def __init__(self, type: str, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return f"Token({self.type}, '{self.value}')"
+
+punctuation_dict = {
+    '(': Token.LPAR,
+    ')': Token.RPAR,
+    '[': Token.LBRACK,
+    ']': Token.RBRACK,
+    ',': Token.COMA,
+    ';': Token.SEMICOL,
+    '+': Token.PLUS,
+    '-': Token.MINUS,
+    '*': Token.MULT,
+    '**': Token.MATMULT,
+    '/': Token.DIV,
+    '%': Token.MOD,
+    '=': Token.EQUAL
+}
+
+class Lexer:
+    def __init__(self, tokens: list):
+        self.tokens = tokens
+        self.pos = -1
+
+    @classmethod
+    def eof_token(cl):
+        return Token(Token.EOF, None)
+
+    @classmethod
+    def str_to_token(cl, s):
+        if s in punctuation_dict:
+            return Token(punctuation_dict[s], s)
+        elif is_literal(s):
+            return Token(Token.LITERAL, s)
+        raise Exception()
+
+    def next_token(self):
+        self.pos += 1
+        if self.pos >= len(self.tokens):
+            return Lexer.eof_token()
+        current_token = self.tokens[self.pos]
+        return self.str_to_token(current_token)
+
+    def get_token(self, i):
+        if self.pos + i >= len(self.tokens):
+            return Lexer.eof_token()
+        return self.str_to_token(self.tokens[self.pos + i])
+
+
 if __name__ == '__main__':
-    toks = re.findall(token_pattern, "funB(y) = 43 * y / (4 % 2 * y)")
-    print(toks)
-    toks = re.findall(token_pattern, "varA = [[2,3];[4,3]]")
-    print(toks)
+    lines = [
+        "funB(y) = 43 * y / (4.1 % 2. * y)",
+        "matA = [[1,2];[3,2];[3,4]]"
+    ]
+    for line in lines:
+        print(f"--- BEGIN LINE {line} ---")
+        toks = tokenize(line)
+        print(toks)
+        lex = Lexer(toks)
+        while True:
+            tok = lex.next_token()
+            print(tok)
+            if tok.type == Token.EOF:
+                break
+        print("--- END LINE ---")
+        print()
